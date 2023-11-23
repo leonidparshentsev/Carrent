98512e0c4015d9fd300a43074d22974dc9cc0b67
Leonid <parshentsevla@gmail.com>
2023-11-23 16:38:51 +0300

    add loader to car model card image

--- src/components/ModelCard/ModelCard.jsx
+++ src/components/ModelCard/ModelCard.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ModelCard.module.scss'
 import Button from '../UI/Button/Button';
 import classNames from 'classnames';
 import { useDispatch } from 'react-redux';
 import { setCarId } from '@/reducer/orderSlice';
 import Link from 'next/link';
-
+import Loader from '../UI/Loader/Loader';
 
 const ModelCard = ({car}) => {
 
     const dispatch = useDispatch();
+    const [isLoaded, setLoaded] = useState(false);
 
     if(car.model === undefined) {
         return (
@@ -24,9 +25,12 @@ const ModelCard = ({car}) => {
     return (
         <div className={styles.model__card}>
             <div className={styles.model__image}>
+                {/* {!isLoaded && <div className={styles.loader}></div>} */}
+                {!isLoaded && <Loader style={{width: '80%', height: '80%'}} />}
                 <img 
                     src={`./images/models/${car.type}/${car.model}.png`} 
-                    alt="Popular model" />
+                    alt="Popular model"
+                    onLoad={() => setLoaded(true)} />
             </div>
             <h4 className={styles.model__title}>{car.model}</h4>
             <p className={styles.model__price}>
